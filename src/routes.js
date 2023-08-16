66b912fb7c41263abcaf6aadd7055aaccee63339
Lucas <lucasmarinhho@hotmail.com>
2023-08-16 14:30:46 -0300

    rotas

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { createUserController } from "./useCases/CreateUser";
+import { deleteUserController } from "./useCases/DeleteUser";
+import { editUserController } from "./useCases/EditUser";
 
 const router = Router()
 
 router.post('/users', (request, response) => {
   return createUserController.handle(request, response);
+});
+
+router.delete('/users/:id', (request, response) => {
+  return deleteUserController.handle(request, response);
+});
+
+router.put('/users/:id', (request, response) => {
+  return editUserController.handle(request, response);
 });
 
 export { router }
